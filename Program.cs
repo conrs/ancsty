eb09ff7249068f3a02db1f55366cacf50f66776f
Matt Connors <matt@con.rs>
2019-08-19 01:16:27 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ancsty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
